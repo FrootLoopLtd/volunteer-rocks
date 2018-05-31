d6985482207d1bab4e31557f6eb2b153ede10936
Mayank Patel <patelpmayank20@gmail.com>
2018-05-31 16:02:19 -0500

    creating database

--- database/db.js
+++ database/db.js
@@ -1,7 +1,21 @@
 var mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/volunteer');
+
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+
+db.on('error', function() {
+  console.log('mongoose connection error');
+});
+
 db.once('open', function() {
   console.log('Volunteer Database is connected');
 });
+
+var volunteerSchema = mongoose.Schema({
+  name: String
+});
+
+var Volunteers = mongoose.model('Volunteers', volunteerSchema);
+
+var vol = new Volunteers({name: 'Linda'});
